5fa74494783145c83dac87c358330ce6bdbd846c
TuanYZ <48976654+alamyazid@users.noreply.github.com>
2022-04-28 01:43:32 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -32,10 +32,10 @@ fi
 mkdir /etc/xray
 mkdir /var/lib/premium-script;
 echo "IP=" >> /var/lib/premium-script/ipvps.conf
-echo "Masukkan Domain Anda, Jika Anda Tidak Memiliki Domain Klik Enter"
-echo "Ketikkan Perintah newhost setelah proses instalasi Script Selesai"
-read -p "Hostname / Domain: " host
-echo "IP="$host >> /var/lib/crot/ipvps.conf
+#echo "Masukkan Domain Anda, Jika Anda Tidak Memiliki Domain Klik Enter"
+#echo "Ketikkan Perintah newhost setelah proses instalasi Script Selesai"
+#read -p "Hostname / Domain: " host
+echo "IP="$host >> /var/lib/premium-script/ipvps.conf
 echo "$host" >> /etc/xray/domain
 #install Domain
 wget https://raw.githubusercontent.com/alamyazid/kalimantan-rev/main/install/cf.sh && chmod +x cf.sh && ./cf.sh
